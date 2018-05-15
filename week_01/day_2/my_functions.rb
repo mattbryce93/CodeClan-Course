231fa9e415cabf456e1464408e925c6a70db809d
Matthew Bryce <mattbryce93@outlook.com>
2018-05-15 14:14:39 +0100

    Excercises Imported

--- week_01/day_2/my_functions.rb
+++ week_01/day_2/my_functions.rb
@@ -1,37 +1,45 @@
-def greet(name)
-  return "Hey" + name
-end
-          # Name cannot begin with a number or an uppercase letter.
-          # Special characters are allowed and imply functionality.
-          # ?greet() implies it returns a boolean for example!
-          # Code outside the function cannot access variables within
-          # It compartmentalizes the Code
-
-p greet("Bob")
-
-def myFunction(parameter)
-  return parameter
-end
-
-p myFunction('argument')
-
-p myFunction(12345)
-
-# def new_greet(name, time_of_day)
-#   return "Good " + time_of_day + ", " + name # this is concantenation
+# def greet(name)
+#   return "Hey" + name
 # end
-
-# p new_greet('Bob', 'morning')
-
-
-# def newer_greet(name, time_of_day)
-#   return "Good #{time_of_day}, #{name}" # this is interpolation
+#           # Name cannot begin with a number or an uppercase letter.
+#           # Special characters are allowed and imply functionality.
+#           # ?greet() implies it returns a boolean for example!
+#           # Code outside the function cannot access variables within
+#           # It compartmentalizes the Code
+#
+# p greet("Bob")
+#
+# def myFunction(parameter)
+#   return parameter
 # end
+#
+# p myFunction('argument')
+#
+# p myFunction(12345)
+#
+# # def new_greet(name, time_of_day)
+# #   return "Good " + time_of_day + ", " + name # this is concantenation
+# # end
+#
+# # p new_greet('Bob', 'morning')
+#
+#
+# # def newer_greet(name, time_of_day)
+# #   return "Good #{time_of_day}, #{name}" # this is interpolation
+# # end
+#
+# # p newer_greet('Bob', 'morning')
+#
+# def greet_upcase(name, time_of_day)
+#   return "Good #{time_of_day}, #{name.capitalize}!"
+# end
+#
+# p greet_upcase('bob', 'morning')
 
-# p newer_greet('Bob', 'morning')
-
-def greet_upcase(name, time_of_day)
-  return "Good #{time_of_day}, #{name.capitalize}!"
+def greet(name, time_of_day)
+  return "Good #{time_of_day}, #{name}"
 end
 
-p greet_upcase('bob', 'morning')
+def add(first_number, second_number)
+  return first_number + second_number
+end
