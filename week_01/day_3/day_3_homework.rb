6ff217498b5ae7fe5ceba4c31add92cb1762b378
Matthew Bryce <mattbryce93@outlook.com>
2018-05-16 16:34:45 +0100

    Day 3 homework Exercise A completed

--- week_01/day_3/day_3_homework.rb
+++ week_01/day_3/day_3_homework.rb
@@ -5,15 +5,60 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Ha
 
 # Complete these tasks:
 # Add "Edinburgh Waverley" to the end of the array
+
+stops.push("Edinburgh Waverley")
+p stops
+
 # Add "Glasgow Queen St" to the start of the array
+
+stops.unshift("Glasgow Queen St")
+p stops
+
 # Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
+
+stops.insert(4, "Polmont")
+p stops
+
 # Work out the index position of "Linlithgow"
+
+p stops.index("Linlithgow")
+
 # Remove "Livingston" from the array using its name
+
+stops.delete("Livingston")
+p stops
+
 # Delete "Cumbernauld" from the array by index
+
+stops.delete_at(2)
+p stops
+
 # How many stops there are in the array?
+
+p stops.length
+
 # How many ways can we return "Falkirk High" from the array?
+
+p stops[2]
+for stop in stops
+  if stop == "Falkirk High"
+    p "This is #{stop}."
+  else
+    p "This is not Falkirk High this is #{stop}."
+  end
+end
+p stops[2..2]
+
+
 # Reverse the positions of the stops in the array
+
+p stops.reverse
+
 # Print out all the stops using a for loop
+
+for a in stops
+  p "#{a}"
+end
 
 # Exercise B
 # Given the following data structure:
--- week_01/day_3/lesson_1_arrays.rb
+++ week_01/day_3/lesson_1_arrays.rb
@@ -37,14 +37,24 @@ p fruits
 fruits.shift() # takes the first element off of the array, 'apple' in this case
 p fruits
 
-fruits.unshift('apple') # puts 'apple' ontot the start of the Array
+fruits.unshift('apple') # puts 'apple' onto the start of the Array
 p fruits
+
+p fruits.index('grape') # prints the index posistion of the 'grape' value. In this case 2.
+
+p fruits.delete('orange') # deletes any value matching 'orange'
+
+p fruits.delete_at(2) # deletes the value in index 2
+
+p fruits.length # prints the number of values in the array
 
 fruits_numbers_arr = ['apple', 1, 'orange', 5] # Ruby allows arrays of diffirent data types in this case strings and integers
 p fruits_numbers_arr
 
 array_inception = [1, 2, 3, 4, [5, 6, 7]] # you can even have arrays within arrays!
 p array_inception
+
+
 
 p fruits
 
