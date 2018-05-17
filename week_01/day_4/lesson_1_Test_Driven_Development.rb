5134cf6238d923c9ff0de00f70ed3eae3c3de604
Matthew Bryce <mattbryce93@outlook.com>
2018-05-17 10:36:13 +0100

    Day 4 lesson 1 file creation

---
+++ week_01/day_4/lesson_1_Test_Driven_Development.rb
@@ -0,0 +1,2 @@
+# Test Driven development (TDD) is a method of development that relies on creating tests for code you intend on writing
+# The key is to write tests for functions, see the functions fail and adjust the function to pass the tests
