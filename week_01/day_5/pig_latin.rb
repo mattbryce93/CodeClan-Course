ae3196204024a7e3abed8bd9eee072b15a9b8c3d
Matthew Bryce <mattbryce93@outlook.com>
2018-05-18 10:49:30 +0100

    Pig latin exercise start

---
+++ week_01/day_5/pig_latin.rb
@@ -0,0 +1,14 @@
+def pigLatin(sentence)
+
+  result = ""
+
+  # split up the string into separate strings
+  # iterate over the array
+  # for each word split them into letters
+  # remove the first letter of the word
+  # add that letter to the end of the word
+  # add 'ay' to the end if the word
+  # concantenate words into reassembled sentence
+  # return new sentence
+
+end
