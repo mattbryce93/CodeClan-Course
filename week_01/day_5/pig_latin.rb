229efd0cfa6f1237a0ab26dd680d37231e36ffde
Matthew Bryce <mattbryce93@outlook.com>
2018-05-18 11:23:02 +0100

    completed pig Latin exercise

--- week_01/day_5/pig_latin.rb
+++ week_01/day_5/pig_latin.rb
@@ -3,12 +3,45 @@ def pigLatin(sentence)
   result = ""
 
   # split up the string into separate strings
+
+  words = sentence.split()
+  #p words
   # iterate over the array
-  # for each word split them into letters
-  # remove the first letter of the word
-  # add that letter to the end of the word
-  # add 'ay' to the end if the word
-  # concantenate words into reassembled sentence
+
+  for word in words
+
+    # for each word split them into letters
+
+    letters = word.split(//)
+    first_letter = letters[0]
+
+    # remove the first letter of the word
+
+    letters.shift().downcase()
+
+    # add that letter to the end of the word
+
+    letters.push(first_letter)
+
+    # add 'ay' to the end if the word
+
+    letters.push("ay ")
+
+    # concantenate words into reassembled sentence
+
+    result += letters.join
+
+    # capitalize the strings first letter
+
+    result.capitalize!()
+  end
+
+  # remove the extra space at the end of the string
+
+  result.chop!()
+
   # return new sentence
+
+  return result
 
 end
