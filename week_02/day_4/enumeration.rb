290becb40df0e07c28d9e5cb0a23e2428797ad0f
Matthew Bryce <mattbryce93@outlook.com>
2018-05-24 09:49:32 +0100

    Week 2 Day 4 Lesson 1 started

---
+++ week_02/day_4/enumeration.rb
@@ -0,0 +1,23 @@
+chickens = ["Maigret", "John", "Louise"]
+
+# for chicken in chickens
+#   p chicken
+# end
+
+
+# chickens.each { |chicken| p chicken }
+
+
+# chickens.each do |chicken|
+#   shouting_chicken = chicken.upcase()
+#   p shouting_chicken
+# end
+
+
+# name_lengths = []
+# chickens.each {|chicken| name_lengths << chicken.length()}
+# p name_lengths
+
+
+# name_lengths = chickens.map { |chicken| chicken.length() }
+# p name_lengths
