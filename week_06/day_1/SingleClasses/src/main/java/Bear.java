b5235736622fd720b3f0a651b465dd292689816a
Matthew Bryce <mattbryce93@outlook.com>
2018-06-18 10:52:31 +0100

    Completed GetName Java exercise

---
+++ week_06/day_1/SingleClasses/src/main/java/Bear.java
@@ -0,0 +1,11 @@
+public class Bear {
+    String name;
+
+    public Bear(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+}
---
+++ week_06/day_1/SingleClasses/src/main/java/Runner.java
@@ -0,0 +1,10 @@
+public class Runner {
+    public static void main(String[] args) {
+        Bear bear = new Bear("Baloo");
+        String name = bear.getName();
+
+        
+
+        System.out.println(name);
+    }
+}
