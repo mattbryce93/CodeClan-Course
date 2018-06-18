74acf4ae38245ee29882873b19fb96a18da18d04
Matthew Bryce <mattbryce93@outlook.com>
2018-06-18 15:41:10 +0100

    Completed Week 6 Day 1

---
+++ week_06/day_1/planets_lab/src/main/java/Planet.java
@@ -0,0 +1,22 @@
+public class Planet {
+    private String name;
+    private int size;
+
+    public Planet(String name, int size){
+        this.name = name;
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void explode() {
+        System.out.println("Boom! " + this.name  + " has exploded.");
+    }
+
+}
---
+++ week_06/day_1/planets_lab/src/main/java/Runner.java
@@ -0,0 +1,8 @@
+public class Runner {
+    public static void main(String[] args) {
+        Planet mars = new Planet ("Mars", 908973);
+
+        System.out.println(mars.getName() + " is " + mars.getSize());
+        mars.explode();
+    }
+}
