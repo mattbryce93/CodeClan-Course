eedfd75601745bcdc099b93d95303c2989697cca
Matthew Bryce <mattbryce93@outlook.com>
2018-06-19 13:25:29 +0100

    Week6 Day2 started afternoon lesson

--- week_06/day_2/WordCollection/src/test/java/NumberListTest.java
+++ week_06/day_2/WordCollection/src/test/java/NumberListTest.java
@@ -1,2 +1,43 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class NumberListTest {
+
+    private NumberList myNumbers;
+
+    @Before
+    public void setup(){
+        ArrayList<Integer> testNumbers = new ArrayList();
+        testNumbers.add(1);
+        testNumbers.add(2);
+        testNumbers.add(3);
+        testNumbers.add(4);
+        testNumbers.add(5);
+        myNumbers = new NumberList(testNumbers);
+    }
+
+    @Test
+    public void hasNumberOfEntries(){
+        assertEquals(5, myNumbers.getNumberCount());
+    }
+
+    @Test
+    public void canAddToList(){
+        myNumbers.addNumber(12);
+        assertEquals(6, myNumbers.getNumberCount());
+    }
+
+    @Test
+    public void canGetFirstNumber(){
+        assertEquals(1, myNumbers.getNumberAtIndex(0));
+    }
+
+    @Test
+    public void canGetTotal(){
+        assertEquals(15, myNumbers.getTotal());
+    }
 }
