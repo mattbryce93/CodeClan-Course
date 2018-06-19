2bf89d5b51986e4c43cd57808122f3419270c4ae
Matthew Bryce <mattbryce93@outlook.com>
2018-06-19 15:46:07 +0100

    Completed Week 6 Day 2 lessons

--- week_06/day_2/MultipleClasses/src/main/java/Bear.java
+++ week_06/day_2/MultipleClasses/src/main/java/Bear.java
@@ -1,7 +1,23 @@
+import java.util.ArrayList;
+
 public class Bear {
     private String name;
+    private ArrayList<Salmon> belly;
 
     public Bear(String name){
         this.name = name;
+        this.belly = new ArrayList<Salmon>();
+    }
+
+    public int foodCount() {
+        return this.belly.size();
+    }
+
+    public void sleep() {
+        this.belly.clear();
+    }
+
+    public void eatFishFromRiver(River river) {
+        this.belly.add(river.removeFish());
     }
 }
