44fcd2899bd372f662c3b54815aa555593dfb9f5
Matthew Bryce <mattbryce93@outlook.com>
2018-06-21 10:10:01 +0100

    Started Week6 Day4 Lesson1

---
+++ week_06/day_4/inheritance_start_point/src/test/java/InstructorTest.java
@@ -0,0 +1,40 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class InstructorTest {
+
+    Instructor instructor;
+
+    @Before
+    public void before(){
+        instructor = new Instructor("John", "G4");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("John", instructor.getName());
+    }
+
+    @Test
+    public void hasCohort(){
+        assertEquals("G4", instructor.getCohort());
+    }
+
+    @Test
+    public void canChangeName(){
+        instructor.setName("Steve");
+        assertEquals("Steve", instructor.getName());
+    }
+    @Test
+    public void canChangeCohort(){
+        instructor.setCohort("G6");
+        assertEquals("G6", instructor.getCohort());
+    }
+
+    @Test
+    public void canTalk(){
+        assertEquals("I love Java", instructor.talk("Java"));
+    }
+}
