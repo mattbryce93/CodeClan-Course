ee2e6c29786b9e1f2ca3902e2bec9e74932acf11
Matthew Bryce <mattbryce93@outlook.com>
2018-07-24 09:25:56 +0100

    edited out commented lines

--- week_11/day_1/js_play/objects.js
+++ week_11/day_1/js_play/objects.js
@@ -1,45 +1,45 @@
-// var myPerson = {
-//   name: 'Guybrush',
-//   age: 32,
-//   weapon: 'cutlass',
-//   talk: function(){
-//     console.log("Shiver me timbers, it's me: " + this.name);
-//   }
-// };
-// //
-// // console.log({myPerson});
-// //
-// // console.log(myPerson.weapon);
-//
-// myPerson.name = 'Guybrush Threepwood';
-// myPerson.age = 1;
-// myPerson.occupation = 'mighty pirate';
-//
-// // console.log({myPerson});
-// //
-// // myPerson.talk();
-//
-// var object = {};
-//
-// var anotherObject = Object.create(null);
-//
-// var yetAnotherObject = new Object();
-// //
-// // console.log(myPerson['name']);
-//
-// var keyToAccess = 'age';
-//
-// // console.log(myPerson[keyToAccess]);
-//
-// var keys = Object.keys(myPerson);
-//
-// for (var key of keys){
-//   console.log('key:', key, 'value', myPerson[key]);
-// }
-//
-// console.log(keys);
+var myPerson = {
+  name: 'Guybrush',
+  age: 32,
+  weapon: 'cutlass',
+  talk: function(){
+    console.log("Shiver me timbers, it's me: " + this.name);
+  }
+};
 
-// Create a bear object that has a type, a name, a belly and an eat method. The eat method should add something to the bear's belly.
+console.log({myPerson});
+
+console.log(myPerson.weapon);
+
+myPerson.name = 'Guybrush Threepwood';
+myPerson.age = 1;
+myPerson.occupation = 'mighty pirate';
+
+console.log({myPerson});
+
+myPerson.talk();
+
+var object = {};
+
+var anotherObject = Object.create(null);
+
+var yetAnotherObject = new Object();
+
+console.log(myPerson['name']);
+
+var keyToAccess = 'age';
+
+console.log(myPerson[keyToAccess]);
+
+var keys = Object.keys(myPerson);
+
+for (var key of keys){
+  console.log('key:', key, 'value', myPerson[key]);
+}
+
+console.log(keys);
+
+```Create a bear object that has a type, a name, a belly and an eat method. The eat method should add something to the bear's belly.```
 
 var bear = {
   type: "Grizzly",
@@ -50,20 +50,20 @@ var bear = {
   }
 };
 
-// console.log({bear});
+console.log({bear});
 
 bear.eat("Salmon");
 
-// console.log({bear});
-//
-// console.log(bear.belly);
+console.log({bear});
+
+console.log(bear.belly);
 
 
-// ```Define an anonymous function expression that takes two arguments:
-//
-// an object, for example, { name: 'Wojtek', age: 30 }
-// a string, for exmaple, 'name'
-// Your function should return true if the given string is a key on the given object and false if the object does not have a key that matches the string. Store this function in an appropriately named variable to invoke it.```
+```Define an anonymous function expression that takes two arguments:
+
+an object, for example, { name: 'Wojtek', age: 30 }
+a string, for exmaple, 'name'
+Your function should return true if the given string is a key on the given object and false if the object does not have a key that matches the string. Store this function in an appropriately named variable to invoke it.```
 
 var checkForKey = function(object, string){
 
@@ -74,12 +74,12 @@ var checkForKey = function(object, string){
     }
   }
 
-  // Secondary way using in loop, useful for hashes.
-  // for (var key in object){
-  //   if (key === string){
-  //     return true;
-  //   }
-  // }
+  Secondary way using in loop, useful for hashes.
+  for (var key in object){
+    if (key === string){
+      return true;
+    }
+  }
 
   return false;
 }
